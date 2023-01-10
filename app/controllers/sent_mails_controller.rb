f5c0fdc68ee350baf08dac645988b4aa7b19f274
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2023-01-10 12:32:06 -0300

    added max mails validation

--- app/controllers/sent_mails_controller.rb
+++ app/controllers/sent_mails_controller.rb
@@ -1,3 +1,5 @@
+require_relative '../errors/max_mails_error.rb'
+
 class SentMailsController < ApplicationController
   before_action :set_sent_mail, only: %i[ show update destroy ]
   load_and_authorize_resource
@@ -16,12 +18,16 @@ class SentMailsController < ApplicationController
 
   # POST /mails
   def create
+    @user_mails = SentMail.where(user: current_user["id"])
+
     @sent_mail = SentMail.new(sent_mail_params)
     @sent_mail.user = current_user
 
     if @sent_mail.save
       render json: @sent_mail, status: :created, location: @sent_mail
-    end
+    else
+      render json: @sent_mail.errors, status: :unprocessable_entity
+  end
   end
 
   # DELETE /mails/1
--- app/models/sent_mail.rb
+++ app/models/sent_mail.rb
@@ -1,3 +1,14 @@
 class SentMail < ApplicationRecord
   belongs_to :user
+  validates :receiver, format: { with: URI::MailTo::EMAIL_REGEXP } , presence: true
+  validates_presence_of :user, :subject, :body
+  validate :validate_user_max_mails
+
+  def validate_user_max_mails
+    @user_mails = SentMail.where(user: user["id"], created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    max_mails = 1000
+    if @user_mails.count >= max_mails
+      errors.add(:user, "You can't send more than #{max_mails} mails per day")
+    end
+  end
 end
